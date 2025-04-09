cf4e939d52b5e35519de5e8c2bb343667f5deae4
Palak Jaiswal <palakj.ug23.cs@nitp.ac.in>
2025-04-10 00:08:47 +0530

    commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+
+export default {
+    providers: [
+      {
+        domain: "https://model-viper-56.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ src/app/(root)/_components/HeaderProfileBtn.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import { User } from 'lucide-react'
+import React from 'react'
+
+function HeaderProfileBtn() {
+  return (
+     <>
+      <UserButton>
+        <UserButton.MenuItems>
+          <UserButton.Link
+            label="Profile"
+            labelIcon={<User className="size-4" />}
+            href="/profile"
+          />
+        </UserButton.MenuItems>
+      </UserButton>
+
+      <SignedOut>
+        <SignInButton />
+
+      </SignedOut>
+     
+     </>
+  )
+}
+
+export default HeaderProfileBtn
