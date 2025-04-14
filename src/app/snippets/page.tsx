1c64d5179664b7bb25071111642f96b1166821e4
Palak Jaiswal <palakj.ug23.cs@nitp.ac.in>
2025-04-14 23:53:42 +0530

    commit

--- src/app/snippets/page.tsx
+++ src/app/snippets/page.tsx
@@ -21,6 +21,15 @@ function SnippetsPage() {
     );
 
   }
+  return (
+    <div className='min-h-screen bg-[#0a0a0f] '>
+      <NavigationHeader />
+      <div></div>
+
+
+    </div>
+
+  )
 
  
 }
