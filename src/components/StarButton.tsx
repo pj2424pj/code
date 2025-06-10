43c6415c21e0111e2ae7787a110270fefd2e4506
Palak Jaiswal <palakj.ug23.cs@nitp.ac.in>
2025-06-10 22:43:55 +0530

    commit

--- src/components/StarButton.tsx
+++ src/components/StarButton.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { Id } from '../../convex/_generated/dataModel'
 
-function StarButton() {
+function StarButton({ snippetId }: { snippetId: Id<"snippets"> }) {
   return (
     <div>StarButton</div>
   )
