b31169439317db03c9a6cddc6d9e807aaf53cac4
Palak Jaiswal <palakj.ug23.cs@nitp.ac.in>
2025-06-11 13:12:47 +0530

    commit

--- src/app/snippets/[id]/_components/CodeBlock.tsx
+++ src/app/snippets/[id]/_components/CodeBlock.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function CodeBlock() {
+function CodeBlock({ language, code }: { language: string; code: string }) {
   return (
     <div>CodeBlock</div>
   )
--- src/app/snippets/[id]/_components/CommentContent.tsx
+++ src/app/snippets/[id]/_components/CommentContent.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import CodeBlock from './CodeBlock';
 
 function CommentContent({ content }: { content: string }) {
+   const parts = content.split(/(```[\w-]*\n[\s\S]*?\n```)/g);
+
   return (
-    <div>CommentContent</div>
-  )
+    <div className="max-w-none text-white">
+      {parts.map((part, index) => {
+        if (part.startsWith("```")) {
+          const match = part.match(/```([\w-]*)\n([\s\S]*?)\n```/);
+
+          if (match) {
+            const [, language, code] = match;
+            return <CodeBlock language={language} code={code} key={index} />;
+          }
+        
+        }
+        return part.split("\n").map((line, lineIdx) => (
+          <p key={lineIdx} className="mb-4 text-gray-300 last:mb-0">
+            {line}
+          </p>
+        ));
+      })}
+    </div>
+  );
 }
 
-export default CommentContent
+export default CommentContent;
