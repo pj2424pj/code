b4679a362360474e47789accfff5c1aba95cfd64
Palak Jaiswal <palakj.ug23.cs@nitp.ac.in>
2025-06-11 15:11:23 +0530

    commit

--- src/app/snippets/[id]/_components/CodeBlock.tsx
+++ src/app/snippets/[id]/_components/CodeBlock.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import CopyButton from "./CopyButton";
+import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-function CodeBlock({ language, code }: { language: string; code: string }) {
+const CodeBlock = ({ language, code }: { language: string; code: string }) => {
+  const trimmedCode = code
+    .split("\n")
+    .map((line) => line.trimEnd())
+    .join("\n");
+
   return (
-    <div>CodeBlock</div>
-  )
-}
+    <div className="my-4 bg-[#0a0a0f] rounded-lg overflow-hidden border border-[#ffffff0a]">
+      <div className="flex items-center justify-between px-4 py-2 bg-[#ffffff08]">
+        <div className="flex items-center gap-2">
+          <img src={`/${language}.png`} alt={language} className="size-4 object-contain" />
+          <span className="text-sm text-gray-400">{language || "plaintext"}</span>
+        </div>
+        <CopyButton code={trimmedCode} />
+      </div>
 
-export default CodeBlock
+      <div className="relative">
+        <SyntaxHighlighter
+          language={language || "plaintext"}
+          style={atomOneDark}
+          customStyle={{
+            padding: "1rem",
+            background: "transparent",
+            margin: 0,
+          }}
+          showLineNumbers={true}
+          wrapLines={true}
+        >
+          {trimmedCode}
+        </SyntaxHighlighter>
+      </div>
+    </div>
+  );
+};
+
+export default CodeBlock;
